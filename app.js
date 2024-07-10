0689c97694df44036a537c94a0e330f6746f1f8a
Mahyar Jalalvand <mahyarjalalvand99@gmail.com>
2024-07-10 15:42:00 +0330

    .

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEYS = "1de427fd7c85dc7ba2884dd9b8377662";
-
 const searchInput = document.querySelector("input")
 const searchButton = document.querySelector("button")
 const weatherContainer = document.getElementById("weather");
-
 const getCurrentWeatherByName = async (city) => {
     const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEYS}&units=metric`;
     const response = await fetch(url);
     const json = response.json()
-    return json
-}
+    return json;
+};
 const renderCurrentWeather = (data) => {
     const weatherJSX = `
         <h1>${data.name},${data.sys.country}</h1>
@@ -20,8 +18,8 @@ const renderCurrentWeather = (data) => {
             <p>${Math.round(data.main.temp)} °C</p>
             </div>
         <div id="info">
-            <p>Humidity :<span>${data.main.humidity}</span></p>
-            <p>Humidity :<span>${data.wind.speed}</span></p>
+            <p>Humidity :<span>${data.main.humidity}%</span></p>
+            <p>Wind Speed :<span>${data.wind.speed}</span></p>
         </div>
     `;
     weatherContainer.innerHTML = weatherJSX;
@@ -36,5 +34,4 @@ const searchHandler = async () => {
     const currentData = await getCurrentWeatherByName(cityName);
     renderCurrentWeather(currentData)
 }
-
 searchButton.addEventListener("click", searchHandler)
